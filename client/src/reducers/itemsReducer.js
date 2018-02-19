dfd5a3b1a90dffa4eb4c93cd5482e4b1735624f9
Robin Pahlman <robin.pahlman@gmail.com>
2018-02-19 11:02:02 -0500

    add case get item in itemsReducer

--- client/src/reducers/itemsReducer.js
+++ client/src/reducers/itemsReducer.js
@@ -2,13 +2,14 @@
 import {GET_ITEMS, GET_ITEM} from '../actions/types';
 
 const initialState = {
-  all: []
+  all: [],
+  item: null
 };
 
 export default function itemsReducer(state = initialState, action) {
   switch (action.type) {
     case GET_ITEMS:
-      return { ...state, all: action.payload.data };
+      return { ...state, all: action.payload.data  };
       //return Object.assign({}, state, {all: action.payload.data}) 
     case GET_ITEM:
       return { ...state, item: action.payload.data }; 
