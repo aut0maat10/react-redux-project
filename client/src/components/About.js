5b10571c427401380413cbb34dc2b57c6310ed91
Robin Pahlman <robin.pahlman@gmail.com>
2018-02-20 14:58:16 -0500

    add react-bootstrap, add jumbotron to About page

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import { Grid, Row, Col, Jumbotron, Button } from 'react-bootstrap';
 
 class About extends Component {
   render() {
     return (
-      <div className="container">
-        <h1>About</h1>
-      </div>
+      <Grid>
+       <Row className="show-grid">
+        <Col md={12}>
+          <Jumbotron>
+            <h1>Hello, world!</h1>
+            <p>
+              This is my React - Redux project for Flatiron School. It's a simple items list with a Redux form to create a new item with a description. The app utilizes a Rails API backend, and sends GET and POST requests triggered by Redux actions to the API, persisting new data, and fetching and displaying data from the database in the React frontend. The app is a work in progress, and I intend to use it as a learning project, adding new functionality along the way. 
+            </p>
+            <p>
+              <Button bsStyle="primary" href="/">Home</Button>
+            </p>
+          </Jumbotron>
+        </Col>
+       </Row>
+     </Grid>
     );
   }
 }
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,14 +1,73 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+//import { NavLink } from 'react-router-dom';
+import { Nav, NavItem } from 'react-bootstrap';
+
+
+// const link = {
+//   width: '100px',
+//   padding: '12px',
+//   margin: '0 6px 6px',
+//   background: 'blue',
+//   textDecoration: 'none',
+//   color: 'white',
+// }
+
+
+// function handleSelect(selectedKey) {
+//   alert(`selected ${selectedKey}`);
+// }
 
 const NavBar = () => {
   return (
     <div className="container">
-      <NavLink to="/" exact>Home</NavLink>
-      <NavLink to="/about" exact>About</NavLink>
-      <NavLink to="/items/new" exact>Create</NavLink>
+    <Nav bsStyle="pills" activeKey={1} >
+      <NavItem eventKey={1} href="/">
+        Home
+    </NavItem>
+      <NavItem eventKey={1} href="/about">
+        About
+    </NavItem>
+      <NavItem eventKey={1} href="/items/new">
+        Create
+    </NavItem>
+    </Nav>
     </div>
   )
 }
 
-export default NavBar;
+export default NavBar; 
+
+/* Add basic styling for NavLinks */
+
+
+/* add the navbar component */
+// const Navbar = () =>
+//   <div>
+//     <NavLink
+//       to="/"
+//       /* set exact so it knows to only set activeStyle when route is deeply equal to link */
+//       exact
+//       /* add styling to Navlink */
+//       style={link}
+//       /* add prop for activeStyle */
+//       activeStyle={{
+//         background: 'darkblue'
+//       }}
+//     >Home</NavLink>
+//     <NavLink
+//       to="/about"
+//       exact
+//       style={link}
+//       activeStyle={{
+//         background: 'darkblue'
+//       }}
+//     >About</NavLink>
+//     <NavLink
+//       to="/login"
+//       exact
+//       style={link}
+//       activeStyle={{
+//         background: 'darkblue'
+//       }}
+//     >Login</NavLink>
+//   </div>;
