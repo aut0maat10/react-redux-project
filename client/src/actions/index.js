cf2279cec226c8bffc1dfc08fd2522615d04fe2b
Robin Pahlman <robin.pahlman@gmail.com>
2018-02-21 16:23:05 -0500

    clean up code in actions/index

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,8 +1,9 @@
-import {GET_ITEMS, CREATE_ITEM } from './types';
+import { GET_ITEMS, CREATE_ITEM } from './types';
 import axios from 'axios';
 
 const API_URL = "http://localhost:3001/api/v1";
 
+//get items from Rails API
 export function getItems() {
   return dispatch => {
     axios.get(`${API_URL}/items`).then(data => {
@@ -14,6 +15,7 @@ export function getItems() {
   }
 }
 
+// create item
 export function createItem(props) {
   console.log('in createItem()')
   debugger
@@ -23,41 +25,6 @@ export function createItem(props) {
     payload: request
   };
 }
-
-
-// export function getItems() {
-//   return function(dispatch) {
-//   dispatch({type: LOADING_ITEMS})
-//   return fetch(`${API_URL}/items`)
-//     .then(res => {
-//       return res.json()
-//     }).then(responseJson => {
-//       dispatch({
-//         type: GET_ITEMS,
-//         payload: responseJson
-//       })
-//     })
-//   }
-// }
-
-// export function deleteItem(id) {
-//   const request = axios.delete(`${API_URL}/items/${id}`);
-//   return {
-//     type: DELETE_ITEM,
-//     payload: request
-//   };
-// }
-
-// export function getItems() {
-//   axios.get(`${API_URL}/items`)
-//     .then((response) => {
-//     console.log(response.data)
-//     return {
-//       type: GET_ITEMS,
-//       payload: response
-//     }
-//   })
-// }
 
 
 
--- client/src/components/ItemShow.js
+++ client/src/components/ItemShow.js
@@ -2,19 +2,6 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Item from './Item';
  
-//const ItemShow = ({ item }) => {
-
-// const ItemShow = (props) => {
-//   return (
-//     <div className="container">
-//       <h3>{props.item.name}</h3>
-//       <h4>{props.item.price}</h4>
-//       <p>{props.item.description}</p>
-//       <p>{props.item.seller_name}</p>
-//       <p>{props.item.seller_phone}</p>
-//     </div>
-//   )
-// }
 class ItemShow extends Component {
 
   render() {
